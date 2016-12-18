c2d8965b63947371cc859dbbefb97a6cb7ccebf9
sewonist <sewonist@gmail.com>
2016-12-18 15:44:24 +0900

    m

---
+++ 01-grid.glsl
@@ -0,0 +1,24 @@
+#ifdef GL_ES
+precision highp float;
+#endif
+
+uniform float time;
+uniform vec2 mouse;
+uniform vec2 resolution;
+uniform vec3 spectrum;
+uniform sampler2D texture0;
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+uniform sampler2D texture3;
+
+varying vec3 v_normal;
+varying vec2 v_texcoord;
+
+void main(void)
+{
+    vec2 uv = -1. + 2. * v_texcoord;
+    float col = 0.;
+    col += abs(1. / sin(uv.x * 10.) ) * spectrum.y * 20.;
+    col += abs(1. / sin(uv.y * 10.) ) * spectrum.x * 20.;
+    gl_FragColor = vec4(col, col, col, 1.0);
+}
